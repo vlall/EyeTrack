b951461cd9be3b418e6659f7aa7fbe311a1aeaaf
V <vlall@users.noreply.github.com>
2014-04-25 21:05:28 -0400

    Update XlsReader.py

--- XlsReader.py
+++ XlsReader.py
@@ -15,7 +15,6 @@ therefore we convert the matrix:
 [ x1 y1 t1 ]
 [ x2 y2 t2 ]
 [ x3 y3 t3 ]
-
 Into a column vector:
 [ x1 ]
 [ y1 ]
@@ -79,7 +78,12 @@ d=1
 # Starting point for y-coordinate starting cell
 e=6
 f=1
-
+# subject
+g=0
+h=1
+# image type
+j=3
+k=1
 # Loop through and make a 2-d array (x values for each image)
 # 25x(24 minus the 2 mistrials)= 25*23- this gives you the number of images shown. 
 # I did (24*23-1) so i wouldn't go over the index bound (549). I substract another 1 to stop before the final cells
@@ -98,6 +102,10 @@ for i in range(548):
 		d+=1
 		f+=1
 	else:
+		listinner.append(first_sheet.cell(g,h).value)
+		listinner.append(first_sheet.cell(j,k).value)
+		h+=(len(listinner)-1)/3
+		k+=(len(listinner)-1)/3	
 		listoutter.append([])
 		print(len(listinner))
 		y+=1
@@ -109,9 +117,74 @@ print (listoutter)
 
 # Extend lists to contain the length of the max list. Add 0's to make up for the extra space.
 maxLen = max(map(len, listoutter))
+listoutter.pop()
 for row in listoutter:
     if len(row) < maxLen:
-        row.extend([0 for z in xrange((maxLen) - len(row))])
+    	print ('IMPORTANT: ' + str(row[-1]))
+    	end=row[-2]
+    	#The NN requires NUMERICAL outputs. Therefore, we convert the strings to numbers.
+    	if (end=='PICT001a'):
+    		end=1
+    	elif (end=='PICT002b'):
+    		end=2
+    	elif (end=='PICT003c'):
+    		end=3
+		#elif (end=='PICT004d'):
+    	elif (end=='PICT005a'):
+    		end=4
+    	elif (end=='PICT006b'):
+    		end=5
+    	elif (end=='PICT007c'):
+    		end=6
+    	elif (end=='PICT008d'):
+    		end=7
+    	elif (end=='PICT009a'):
+    		end=8
+    	elif (end=='PICT010b'):
+    		end=9
+    	elif (end=='PICT011c'):
+    		end=10
+    	#elif (end=='PICT012d'):
+    	elif (end=='PICT013a'):
+    		end=11
+    	elif (end=='PICT014b'):
+    		end=12
+    	elif (end=='PICT015c'):
+    		end=13
+    	elif (end=='PICT016d'):
+    		end=14
+    	elif (end=='PICT017a'):
+    		end=15
+    	elif (end=='PICT018b'):
+    		end=16
+    	elif (end=='PICT019c'):
+    		end=17
+    	elif (end=='PICT020d'):
+    		end=18
+    	elif (end=='PICT021a'):
+    		end=19
+    	elif (end=='PICT022b'):
+    		end=20
+    	elif (end=='PICT023c'):
+    		end=21
+    	elif (end=='PICT024d'):  
+    		end=22
+    	end2=row[-1]
+    	if (end2=='FACE'):
+			end2=1
+    	elif (end2=='OUTDOOR'):
+			end2=2
+    	elif (end2=='INDOOR'):
+			end2=3
+    	elif (end2=='OBJECT'):
+			end2=4
+    	elif (end2=='MANIP'):
+			end2=5
+    	row.pop()
+    	row.pop()
+        row.extend([0.0 for z in xrange((maxLen) - len(row))])
+        row.append(end)
+        row.append(end2)
 
 # This is important to know, it will tell us the amount of inputs we will use in the NN.
 # Once we know this, we will work on a topolopy in BackProp.py
@@ -128,7 +201,7 @@ for v in range (len(singleList)-1):
 	ws.write(v, 0, str(singleList[v]))
 wb.save('new346' + '.xls')
 '''
-for r in range(len(listoutter)-1):
-	for col in range (len(listoutter[r])-1):
+for r in range(len(listoutter)):
+	for col in range (len(listoutter[r])):
 		ws.write(r, col, str(listoutter[r][col]))
-wb.save('new346' + '.xls')
+wb.save('new347' + '.xls')
